368660445b1aa0c81ee0131aec43e85642316cf7
Alexys Heffernan <alexysheffernan@gmail.com>
2024-10-02 19:50:28 -0400

    qualified updates

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -3,7 +3,7 @@ const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
-  .route("/movies/:movieId/reviews")
+  .route("/:reviewId")
   .get(controller.read) 
   .put(controller.update) 
   .delete(controller.delete) 
